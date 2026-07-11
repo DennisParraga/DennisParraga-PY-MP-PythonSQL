fc896f2c064ebe40f3cf1c1021d442f14bd876ac
DennisParraga <DennisParraga@github.com>
2026-07-11 16:36:39 +0000

    Test run: 2/3 passed, 1 failed, 0 skipped - 2026-07-11T16:36:39.937Z

--- src/main/lab.py
+++ src/main/lab.py
@@ -17,14 +17,14 @@ def create_dogs_table():
 
 # TODO: Complete insert_dog() by inserting a new dog (provided in the parameters) into the "dogs" table.
 def insert_dog(name, breed, age):
-
-    cursor.execute(f"INSERT INTO dogs(name, breed, age) VALUES({name}, {breed}, {age});")
+    string = f"INSERT INTO dogs(name, breed, age) VALUES({name}, {breed}, {age});"
+    cursor.execute(string)
 
 
 # TODO: Complete select_all_dogs() by selecting all rows from the "dogs" table *and returning them*.
 def select_all_dogs():
-    # cursor.execute("SELECT * FROM dogs;")
+    cursor.execute("SELECT * FROM dogs;")
     # return the rows
     # cursor.fetchall()
     # print(cursor.fetchall())
-    return [1]
+    return cursor.fetchall()
